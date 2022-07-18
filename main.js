9b31f078fa672b5103bfacb03f3f7d85f9132a6c
Facundo <facuc0503@gmail.com>
2022-07-18 11:25:19 -0300

    Subo Api del clima

---
+++ main.js
@@ -0,0 +1,46 @@
+const API_KEY = "f84f7a6ead1a87099c19b6e5c4f6892f";
+
+const fetchData = (position) => {
+  const { latitude, longitude } = position.coords;
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`
+  )
+    .then((response) => response.json())
+    .then((data) => setWeatherData(data));
+};
+
+const setWeatherData = (data) => {
+  console.log(data);
+  const weatherData = {
+    location: data.name,
+    description: data.weather[0].main,
+    humidity: data.main.humidity,
+    pressure: data.main.pressure,
+    temperature: Math.floor(data.main.temp),
+    date: getDate(),
+  };
+  Object.keys(weatherData).forEach((key) => {
+    document.getElementById(key).textContent = weatherData[key];
+  });
+
+  cleanUp();
+};
+
+const cleanUp = () => {
+  let container = document.getElementById("container");
+  let loader = document.getElementById("loader");
+
+  loader.style.display = "none";
+  container.style.display = "flex";
+};
+
+const getDate = () => {
+  let date = new Date();
+  return `${date.getDate()}-${("0" + (date.getMonth() + 1)).slice(
+    -2
+  )}-${date.getFullYear()}`;
+};
+
+const onLoad = () => {
+  navigator.geolocation.getCurrentPosition(fetchData);
+};
